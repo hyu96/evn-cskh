1d455364ef49babbce0c369b8d102c0cdcbd825b
HuyDQ <huydq@i-com.vn>
2023-07-14 19:01:24 +0700

    Init commit

--- src/views/Sanluongdien.vue
+++ src/views/Sanluongdien.vue
@@ -21,7 +21,7 @@ export default {
       
       
       var currentDate = new Date();
-
+        
         var day = String(currentDate.getDate()).padStart(2, '0');
         var month = String(currentDate.getMonth() + 1).padStart(2, '0');
         var year = currentDate.getFullYear();
